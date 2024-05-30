b3ac20eacc0cf7403f85732413d47167223110b8
ambition2k <159018424+ambition2k@users.noreply.github.com>
2024-05-30 21:19:18 +0700

    Create CustomerRepository.cs

---
+++ Jewelry.Data/Repository/CustomerRepository.cs
@@ -0,0 +1,17 @@
+using Jewelry.Data.Base;
+using Jewelry.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jewelry.Data.Repository
+{
+    public class CustomerRepository : GenericRepository<SiCustomer>
+    {
+        public CustomerRepository()
+        {
+        }
+    }
+}
