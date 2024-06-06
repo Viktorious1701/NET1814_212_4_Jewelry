3ccf9e11448f476a526b25c4a8960fcdf051aef5
ThinhNguyenrt <ThinhNPSE183518@fpt.edu.vn>
2024-06-06 07:15:38 +0700

    Update UnitOfWork.cs

--- Jewelry.Data/UnitOfWork.cs
+++ Jewelry.Data/UnitOfWork.cs
@@ -14,6 +14,7 @@ namespace Jewelry.Data
         private CategoryRepository _category;
         private OrderItemRepository _orderItem;
         private CustomerRepository _customer;
+        private CompanyRepository _company;
         private readonly NET1814_212_4_JewelryContext _unitOfWorkContext;
 
         public UnitOfWork()
@@ -49,6 +50,13 @@ namespace Jewelry.Data
                 return (_customer ??= new Repository.CustomerRepository());
             }
         }
+         public CompanyRepository CompanyRepository
+         {
+             get
+             {
+                 return _company ??= new Repository.CompanyRepository();
+             }
+         }
         ////TO-DO CODE HERE/////////////////
 
         #region Set transaction isolation levels
