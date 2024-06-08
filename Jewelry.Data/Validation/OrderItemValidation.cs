af8447b196b5798af0830cba90fc3b7b563a3963
Tan Nguyen <tannd1008@gmail.com>
2024-06-08 21:08:56 +0700

    Add validation

---
+++ Jewelry.Data/Validation/OrderItemValidation.cs
@@ -0,0 +1,46 @@
+﻿using Jewelry.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Jewelry.Data.Validation
+{
+    public class OrderItemValidation
+    {
+        private readonly NET1814_212_4_JewelryContext _context;
+        public bool ValidateOrderItem(OrderItem orderItem)
+        {
+            if(orderItem == null)
+            {
+                return false;
+            }
+            Regex regex = new Regex((@"^\d$"));
+            if (orderItem.Price <= 0)
+            {
+                return false;
+            }
+
+            if (orderItem.Quantity <= 0)
+            {
+                return false;
+            }
+
+            if (!regex.IsMatch(orderItem.ProductId.ToString()) ||
+                !regex.IsMatch(orderItem.OrderItemId.ToString()) || 
+                !regex.IsMatch(orderItem.OrderId.ToString()))
+            {
+                return false;
+            }
+
+            if(_context.FindAsync<OrderItem>(orderItem.OrderItemId) == null)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
